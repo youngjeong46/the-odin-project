64f87371e522e0a6ca54a8cd3b49361bbec2ab2b
Young Jeong <youngjeong46@gmail.com>
2016-06-20 20:44:34 -0700

    Basics done, only about 35 grids before it takes too long to generate

--- etch-a-sketch/jscript.js
+++ etch-a-sketch/jscript.js
@@ -1,37 +1,30 @@
 $(document).ready(function() {
 
-	var $container = $('.container');
-	
+	var $box = $('.box');
+  	createGrid(16);
 
-	function createGrid(numSq) {
+  	$("#button").click(function(){
+  		$("button").attr('value', 'RESET');
+  		$(".container").empty();
+	  	var numSq = prompt("Enter a value between 1 and 40");
+	  	createGrid(numSq);
+	});
+});
+
+function createGrid(numSq) {
 	    for (var i = 1; i <= numSq; i++) {
 	      for (var j = 1; j <= numSq; j++) {
-	        $container.append('<div class="box"></div>');
+	        $('.container').append('<div class="box"></div>');
 	        $('.box').height((960/numSq)-5);
 			$('.box').width((960/numSq)-5);
 	        $('.box').addClass('box_original');
 	      }
 	    }
+	    
+	    $('.box').hover(function(){
+			$(this).addClass('box_hover');
+		});
 	};
-
-  	createGrid(16);
-
-  	var $box = $('.box');
-
-  	$("#button").click(function(){
-  		$("#button").attr('value', 'RESET');
-	  	var numSq = prompt("Enter a value between 1 and 40");
-	  	$('.box').removeClass('box_hover');
-	  	$('.box').remove();
-	  	createGrid(numSq);
-	});
-	
-
-	$box.hover(function() {
-		$(this).addClass('box_hover');
-	});
-
-});
 
 
 
