039af3d3edbc907b9237075a2c37bdf86c5c9767
Young Jeong <youngjeong46@gmail.com>
2016-06-20 20:03:13 -0700

    Reset button set, grid resets successfully, but hover won't work after first and subsequent resetting

--- etch-a-sketch/jscript.js
+++ etch-a-sketch/jscript.js
@@ -1,33 +1,39 @@
 $(document).ready(function() {
-  var $container = $('.container');
 
-  function createGrid(numSq) {
-    for (var i = 1; i <= numSq; i++) {
-      for (var j = 1; j <= numSq; j++) {
-        $container.append('<div class="box"></div>');
-        $('.box').addClass('box_original');
-      }
-    }
-  }
+	var $container = $('.container');
+	
 
-  //createGrid(16);
+	function createGrid(numSq) {
+	    for (var i = 1; i <= numSq; i++) {
+	      for (var j = 1; j <= numSq; j++) {
+	        $container.append('<div class="box"></div>');
+	        $('.box').height((960/numSq)-5);
+			$('.box').width((960/numSq)-5);
+	        $('.box').addClass('box_original');
+	      }
+	    }
+	};
 
-  var $box = $('.box');
+  	createGrid(16);
 
-  $box.hover(function() {
-    $(this).addClass('box_hover');
-  });
-  //{
-  //var random = '#'+Math.floor(Math.random()*16777215).toString(16);
-  // $(this).css("background-color",random);
-  // });
+  	var $box = $('.box');
+
+  	$("#button").click(function(){
+  		$("#button").attr('value', 'RESET');
+	  	var numSq = prompt("Enter a value between 1 and 40");
+	  	$('.box').removeClass('box_hover');
+	  	$('.box').remove();
+	  	createGrid(numSq);
+	});
+	
+
+	$box.hover(function() {
+		$(this).addClass('box_hover');
+	});
 
 });
 
-var click_function = function() {
-  $("#button").attr('value', 'Reset');
-  var numSq = prompt("Enter a value between 1 and 40");
-  console.log(numSq * 2);
-  createGrid(numSq);
-  $('.box').removeClass('box_hover');
-}
+
+
+
+
